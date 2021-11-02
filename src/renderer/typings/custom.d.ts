e7bb763be347e31c9c7ec5f20ea47e034946fc07
Andrew Wei <andrewscwei@gmail.com>
2021-11-01 17:37:21 -0700

    Improve typings

--- src/renderer/typings/custom.d.ts
+++ src/renderer/typings/custom.d.ts
@@ -1,17 +1,31 @@
-declare const __APP_CONFIG__: Record<string, any>
+// Supports importing assets.
+declare module '*.svg'
+declare module '*.jpg'
+declare module '*.png'
+
+// Supports typing for app config.
+declare const __APP_CONFIG__: typeof import('./app.conf').default
+
+// Supports web workers.
+declare module 'worker-loader!*' {
+  class WebpackWorker extends Worker {
+    constructor();
+  }
+
+  export default WebpackWorker
+}
+
+// Supports I18n.
+interface TranslationData { [key: string]: TranslationData | string }
+type TranslationDataDict = Record<string, TranslationData>
+
 declare const __I18N_CONFIG__: Readonly<{
   defaultLocale: string
   locales: string
   dict: TranslationDataDict
 }>
 
-declare module '*.svg'
-declare module '*.jpg'
-declare module '*.png'
-
-type TranslationData = { [key: string]: string | TranslationData }
-type TranslationDataDict = Record<string, TranslationData>
-
+// Custom window properties.
 interface Window {
   __BUILD_NUMBER__: string
   __VERSION__: string
