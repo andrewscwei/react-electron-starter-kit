9e00c521226d49fe61aa1fbf8ae8f79ae74e71a0
Andrew Wei <andrewscwei@gmail.com>
2021-11-16 19:27:56 -0800

    Optimize styles

--- src/renderer/styles/global.ts
+++ src/renderer/styles/global.ts
@@ -3,15 +3,15 @@ import normalize from 'styled-normalize'
 import * as theme from './theme'
 
 export default css`
+  /* stylelint-disable-next-line max-empty-lines */
+  ${normalize}
+
   @font-face {
     font-family: 'RobotoMono';
     src: url('fonts/RobotoMono-Regular.ttf') format('truetype');
     font-style: normal;
     font-weight: 400;
   }
-
-  /* stylelint-disable-next-line max-empty-lines */
-  ${normalize}
 
   html,
   body {
